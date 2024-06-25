7eaee302d466977145751b67519c439f1453619b
FalAut <143469089+FalAut@users.noreply.github.com>
2024-06-25 19:12:11 +0800

    better

--- scripts/Slime_Sling_and_Boots/SlimeSlingAndBoots.js
+++ scripts/Slime_Sling_and_Boots/SlimeSlingAndBoots.js
@@ -1,32 +1,3 @@
-const { $Item } = require("packages/net/minecraft/world/item/$Item");
-const { $ClipContext$Fluid } = require("packages/net/minecraft/world/level/$ClipContext$Fluid");
-const { $HitResult$Type } = require("packages/net/minecraft/world/phys/$HitResult$Type");
-const { $TickEvent$PlayerTickEvent } = require("packages/net/minecraftforge/event/$TickEvent$PlayerTickEvent");
-const { $LivingFallEvent } = require("packages/net/minecraftforge/event/entity/living/$LivingFallEvent");
-const { $PlayerFlyableFallEvent } = require("packages/net/minecraftforge/event/entity/player/$PlayerFlyableFallEvent");
-
-global.SlimeSlingUsing = (itemstack, level, entity, timeLeft) => {
-    if (!entity.onGround()) return;
-
-    let hitResult = $Item.getPlayerPOVHitResult(level, entity, $ClipContext$Fluid.NONE);
-    let timeUsed = itemstack.useDuration - timeLeft;
-    let i = timeUsed / 20;
-
-    i = (i * i + i * 2) / 3;
-    i *= 4;
-
-    if (i > 6) i = 6;
-
-    if (hitResult?.type == $HitResult$Type.BLOCK) {
-        let vec3 = entity.lookAngle.normalize();
-
-        entity.addDeltaMovement(Vec3d(vec3.x() * -i, (vec3.y() * -i) / 3, vec3.z() * -i));
-        addBounceHandler(entity, 0);
-    }
-
-    if (i > 1) entity.playSound("entity.slime.jump_small", 1, 1);
-};
-
 function addBounceHandler(entity, bounce) {
     if (entity.isPlayer() && !entity.isFake()) {
         entity.persistentData.putBoolean("bounce", true);
@@ -37,9 +8,74 @@ function addBounceHandler(entity, bounce) {
     }
 }
 
-global.BounceHandler = (/**@type {$TickEvent$PlayerTickEvent} */ event) => {
-    const { player } = event;
-    if (!player.persistentData.getBoolean("bounce")) return;
+function OnFallWithSlimeBoots(event) {
+    const { entity, distance } = event;
+    if (entity.getItemBySlot("feet") != "kubejs:slime_boots") return;
+
+    if (!entity.crouching && distance > 2) {
+        if (entity.abilities.mayfly) {
+            event.setDistance(distance);
+        } else {
+            event.setDamageMultiplier(0);
+            entity.resetFallDistance();
+        }
+
+        if (entity.level.clientSide) {
+            const motion = entity.deltaMovement;
+
+            entity.setDeltaMovement(Vec3d(motion.x(), motion.y() * -0.9, motion.z()));
+            entity.hasImpulse = true;
+            entity.setOnGround(false);
+        } else {
+            event.setCanceled(true);
+        }
+
+        entity.playSound("minecraft:entity.slime.squish");
+
+        for (let i = 0; i < 8; i++) {
+            const random1 = entity.random.nextFloat() * 6.2831855;
+            const random2 = entity.random.nextFloat() * 0.5 + 0.5;
+            const xOffset = Math.sin(random1) * 0.5 * random2;
+            const yOffset = Math.cos(random1) * 0.5 * random2;
+            entity.level.addParticle("minecraft:item_slime", entity.x + xOffset, entity.y, entity.z + yOffset, 0, 0, 0);
+        }
+        addBounceHandler(entity, entity.deltaMovement.y());
+    }
+}
+
+StartupEvents.registry("item", (event) => {
+    event.create("slime_boots", "boots");
+    event
+        .create("slime_sling")
+        .unstackable()
+        .use(() => true)
+        .useAnimation("bow")
+        .useDuration(() => 72000)
+        .releaseUsing((itemstack, level, entity, timeLeft) => {
+            if (!entity.onGround()) return;
+            let hitResult = itemstack.item.getPlayerPOVHitResult(level, entity, "none");
+            let timeUsed = itemstack.useDuration - timeLeft;
+            let i = timeUsed / 20;
+
+            i = (i * i + i * 2) / 3;
+            i *= 4;
+
+            if (i > 6) i = 6;
+
+            if (hitResult?.type == "block") {
+                let vec3 = entity.lookAngle.normalize();
+
+                entity.addDeltaMovement(Vec3d(vec3.x() * -i, (vec3.y() * -i) / 3, vec3.z() * -i));
+                addBounceHandler(entity, 0);
+            }
+
+            if (i > 1) entity.playSound("entity.slime.jump_small", 1, 1);
+        });
+});
+
+ForgeEvents.onEvent("net.minecraftforge.event.TickEvent$PlayerTickEvent", (event) => {
+    const { player, phase } = event;
+    if (phase != "END" || !player.persistentData.getBoolean("bounce")) return;
 
     if (
         player.abilities.flying ||
@@ -97,67 +133,12 @@ global.BounceHandler = (/**@type {$TickEvent$PlayerTickEvent} */ event) => {
     }
 
     player.persistentData.put("bounceData", bounceData);
-};
-
-global.OnFallWithSlimeBoots = (/**@type {$LivingFallEvent} */ event) => {
-    const { entity, distance } = event;
-    const { level } = entity;
-    if (entity.getItemBySlot("feet") != "kubejs:slime_boots") return;
-
-    if (!entity.crouching && distance > 2) {
-        if (entity.abilities.mayfly) {
-            event.setDistance(distance);
-        } else {
-            event.setDamageMultiplier(0);
-            entity.resetFallDistance();
-        }
-
-        if (level.clientSide) {
-            const motion = entity.deltaMovement;
-
-            entity.setDeltaMovement(Vec3d(motion.x(), motion.y() * -0.9, motion.z()));
-            entity.hasImpulse = true;
-            entity.setOnGround(false);
-        } else {
-            event.setCanceled(true);
-        }
-
-        entity.playSound("minecraft:entity.slime.squish");
-
-        for (let i = 0; i < 8; i++) {
-            const random1 = entity.random.nextFloat() * 6.2831855;
-            const random2 = entity.random.nextFloat() * 0.5 + 0.5;
-            const xOffset = Math.sin(random1) * 0.5 * random2;
-            const yOffset = Math.cos(random1) * 0.5 * random2;
-            level.addParticle("minecraft:item_slime", entity.x + xOffset, entity.y, entity.z + yOffset, 0, 0, 0);
-        }
-        addBounceHandler(entity, entity.deltaMovement.y());
-    }
-};
-
-StartupEvents.registry("item", (event) => {
-    event
-        .create("slime_sling")
-        .unstackable()
-        .use(() => true)
-        .useAnimation("bow")
-        .useDuration(() => 72000)
-        .releaseUsing((itemstack, level, entity, timeLeft) => {
-            global.SlimeSlingUsing(itemstack, level, entity, timeLeft);
-        });
-
-    event.create("slime_boots", "boots");
 });
 
-ForgeEvents.onEvent($TickEvent$PlayerTickEvent, (event) => {
-    if (event.phase != "END") return;
-    global.BounceHandler(event);
+ForgeEvents.onEvent("net.minecraftforge.event.entity.living.LivingFallEvent", (event) => {
+    OnFallWithSlimeBoots(event);
 });
 
-ForgeEvents.onEvent($LivingFallEvent, (event) => {
-    global.OnFallWithSlimeBoots(event);
-});
-
-ForgeEvents.onEvent($PlayerFlyableFallEvent, (event) => {
-    global.OnFallWithSlimeBoots(event);
+ForgeEvents.onEvent("net.minecraftforge.event.entity.player.PlayerFlyableFallEvent", (event) => {
+    OnFallWithSlimeBoots(event);
 });
