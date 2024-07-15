b0b5b5063138e6eb0f30972434953aa3f249b9d9
FalAut <143469089+FalAut@users.noreply.github.com>
2024-07-15 15:35:06 +0800

    fix

--- scripts/Enchantment_Last_Stand/startup_scripts/enchantment_last_stand.js
+++ scripts/Enchantment_Last_Stand/startup_scripts/enchantment_last_stand.js
@@ -3,25 +3,20 @@ let $EnchantmentHelper = Java.loadClass("net.minecraft.world.item.enchantment.En
 ForgeEvents.onEvent("net.minecraftforge.event.entity.living.LivingHurtEvent", (event) => {
     const { entity, amount } = event;
     if (!entity.isPlayer()) return;
-    // Get player's last stand enchantment level
+
     const enchantmentLevels = $EnchantmentHelper.getEnchantmentLevel("kubejs:last_stand", entity);
-    // If player has last stand enchantment
-    if (enchantmentLevels > 0) {
-        // Calculate the xp required for revival
+
+    if (enchantmentLevels > 0 && entity.health - amount < 1) {
         const xpRequired = Math.max(1, (50 * (1 - (entity.health - amount))) / enchantmentLevels);
-        // If player has enough xp
         if (getPlayerXP(entity) >= xpRequired) {
-            // Set player's health to 1 (prevent death)
             entity.setHealth(1);
-            // Deduct the xp from the player
             entity.giveExperiencePoints(-xpRequired);
-            // Cancel the hurt event
+            event.setAmount(0);
             event.setCanceled(true);
         }
     }
 });
 
-// minecraft doesn't update experience properly, so we have to do this
 function sum(n, a0, d) {
     return (n * (2 * a0 + (n - 1) * d)) / 2;
 }
