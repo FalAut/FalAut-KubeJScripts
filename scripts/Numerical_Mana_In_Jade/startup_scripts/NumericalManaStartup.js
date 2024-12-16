31615562ad15805ee7113c13a3e6006faeb90c4b
FalAut <143469089+FalAut@users.noreply.github.com>
2024-12-16 23:18:40 +0800

    1

--- scripts/Numerical_Mana_In_Jade/startup_scripts/NumericalManaStartup.js
+++ scripts/Numerical_Mana_In_Jade/startup_scripts/NumericalManaStartup.js
@@ -1,11 +1,12 @@
 let $BlockEntity = Java.loadClass('net.minecraft.world.level.block.entity.BlockEntity')
 
 JadeEvents.onCommonRegistration((event) => {
-    const manaNbtKeys = ["currentMana", "maxMana", "mana", "manaToGet"];
+    const manaMethods = ["currentMana", "maxMana", "mana", "manaToGet"];
+    
     event.blockDataProvider("kubejs:numerical_mana", $BlockEntity).setCallback((tag, accessor) => {
         const { blockEntity } = accessor;
 
-        manaNbtKeys.forEach((key) => {
+        manaMethods.forEach((key) => {
             if (blockEntity[key] != null) {
                 tag.putInt(key, blockEntity[key]);
             }
